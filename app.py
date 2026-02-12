7820a5632b47088da270d6410018dbc02e53fd7f
pintarkantor-prog <pintarkantor@gmail.com>
2026-02-12 22:31:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def pasang_css_kustom():
         /* MERUBAH TAMPILAN KOLOM/KOTAK INPUT ADEGAN */
         div[data-testid="stNumberInput"] {
             background-color: #161922 !important; /* Warna dasar kotak lebih gelap */
-            border-radius: 5px !important;       /* Sudut melengkung halus */
+            border-radius: 15px !important;       /* Sudut melengkung halus */
             padding: 1px !important;
 
         }
@@ -357,6 +357,7 @@ def utama():
 
 if __name__ == "__main__":
     utama()
+
 
 
 
