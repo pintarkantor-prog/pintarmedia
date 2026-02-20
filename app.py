7ea5286d2b7856c97c5c4538c95e7e256eadd353
pintarkantor-prog <pintarkantor@gmail.com>
2026-02-20 14:46:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1168,7 +1168,7 @@ def tampilkan_tugas_kerja():
             else: st.write("Belum ada riwayat.")
 
     # --- 5. GAJIAN ---
-    if user_sekarang != "dian" and user_sekarang != "tamu" and sekarang.day >= 19:
+    if user_sekarang != "dian" and user_sekarang != "tamu" and sekarang.day >= 1:
         st.divider()
         with st.expander("💰 **KLAIM SLIP GAJI BULAN INI**"):
             try:
@@ -1814,6 +1814,7 @@ def utama():
 # --- BAGIAN PALING BAWAH ---
 if __name__ == "__main__":
     utama()
+
 
 
 
