f12231f71cc8c7a073956160a79f528c9a3238ec
pintarkantor-prog <pintarkantor@gmail.com>
2026-02-25 13:43:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2120,10 +2120,13 @@ def tampilkan_kendali_tim():
                                 """, unsafe_allow_html=True)
                                 
                                 # EMAIL & PASSWORD (2 KOLOM - FONT 15PX)
-                                c1, c2 = st.columns(2)
+                                c1, c2, c3 = st.columns(3)
                                 c1.markdown(f"<p style='margin:10px 0 0 0; font-size:11px; color:#888;'>📧 EMAIL</p><code style='font-size:15px !important; display:block; padding:5px;'>{r['EMAIL']}</code>", unsafe_allow_html=True)
                                 c2.markdown(f"<p style='margin:10px 0 0 0; font-size:11px; color:#888;'>🔑 PASSWORD</p><code style='font-size:15px !important; display:block; padding:5px;'>{r['PASSWORD']}</code>", unsafe_allow_html=True)
-                                
+                                c3.markdown(f"""
+                                    <p style='margin:10px 0 0 0; font-size:11px; color:#888;'>👤 PEMAKAI</p>
+                                    <div style='background:{warna_p}; color:white; padding:4px; border-radius:5px; text-align:center; font-size:12px; font-weight:bold; margin-top:5px;'>
+                                        {pemakai_txt}
                                 st.divider()
                                 
                                 # STATUS, EXPIRED, SISA (3 KOLOM SEJAJAR)
@@ -2489,37 +2492,3 @@ def utama():
 # --- BAGIAN PALING BAWAH ---
 if __name__ == "__main__":
     utama()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
