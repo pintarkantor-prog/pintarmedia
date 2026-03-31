ac67f597c2a542d07aeb74467bc03f94fd2f31b3
pintarkantor-prog <pintarkantor@gmail.com>
2026-03-31 17:38:36 +0700

    Update area_staf.py

--- pages_content/area_staf.py
+++ pages_content/area_staf.py
@@ -47,7 +47,7 @@ def tampilkan_area_staf():
             "sop": [
                 "**Kualitas:** Minimal 1080p Full HD.",
                 "**Ratio:** Format 9:16 (Vertical).",
-                "**Durasi:** Disesuaikan kebututahn.",
+                "**Durasi:** Disesuaikan kebutuhan.",
                 "**Nama file video:** TGL_NAMA_JUDUL.mp4"
             ]
         },
