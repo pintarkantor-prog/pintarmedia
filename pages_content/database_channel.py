321112316ed24e743d86c958acfe5247600b4048
pintarkantor-prog <pintarkantor@gmail.com>
2026-04-02 20:36:37 +0700

    Update database_channel.py

--- pages_content/database_channel.py
+++ pages_content/database_channel.py
@@ -228,7 +228,7 @@ def tampilkan_database_channel():
                                         target_hp = "1"
                                         for h in range(1, 101):
                                             count_sekarang = hp_counts.get(str(h), 0)
-                                            max_slot = 3 if h in [31, 32] else 4
+                                            max_slot = 3 if h in [1, 2, 3, 4, 5, 6, 7, 8, 9] else 4
                                             if count_sekarang < max_slot:
                                                 target_hp = str(h)
                                                 break
