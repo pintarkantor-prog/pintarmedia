6ebb9fa4e295501912e51a74da14ebfd47ce4687
pintarkantor-prog <pintarkantor@gmail.com>
2026-04-13 01:33:04 +0700

    Update area_staf.py

--- pages_content/area_staf.py
+++ pages_content/area_staf.py
@@ -605,14 +605,14 @@ def tampilkan_area_staf():
                     st.write("- **Logout Device Penjual:** Hapus akses semua HP penjual di 'Manage Devices'.")
                     st.write("- Ganti password ke standar kantor langsung setelah login.")
                     
-            st.write("") # Spacing
             with st.container(border=True):
-                st.markdown("#### 🏗️ Produksi & Stok Channel Standby")
-                st.info("💡 **Tugas Utama:** Admin adalah penyedia akun agar operasional tidak berhenti.")
-                st.write("- Mencari akun Google fresh/tua untuk bahan channel baru.")
-                st.write("- Membuat channel-channel baru secara berkala untuk stok standby.")
-                st.write("- Memastikan channel standby sudah siap (setting dasar YT sudah oke).")
-                st.write("- Segera masukkan data channel baru ke sistem agar siap digunakan.")
+                st.markdown("#### 📥 Prosedur Karantina & Pemanasan Akun Baru Beli")
+                st.write("- Akun yang baru beli dicatat di Gsheet terlebih dahulu")
+                st.write("- Di Hari ke-2, Gunakan akun tersebut untuk baca berita di Google secara normal ( *Tetap gunakan Reset IP setiap ganti Akun* ).")
+                st.write("- Di hari ke-3, baru buat Channel YT & sesuaikan nama/foto profile.")
+                st.write("- Input Data Akun ke Database Pintar Media ( *status channel Standby* ).")
+                st.write("- Hapus akun yang sudah di Input di HP Login, lalu bersihkan cache dan restart HP.")
+
 
             st.error("**🚨 PENTING:** Kapasitas maksimal **10 akun per HP**. Dilarang keras login akun baru tanpa melakukan reset IP!")
     
